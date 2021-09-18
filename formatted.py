e0cb9cae7632b8eb391e8ad1edfd2ab0c1d82d59
nikolaevv <starlei165@gmail.com>
2021-09-18 03:36:02 +0300

    feat: Add generating html

--- formatted.py
+++ formatted.py
@@ -1,5 +1,4 @@
-configjs = '''
-import {{createTheme}} from '@material-ui/core/styles';
+configjs = '''import {{createTheme}} from '@material-ui/core/styles';
 
 const theme = createTheme({{
 	palette: {{
@@ -16,3 +15,18 @@ const theme = createTheme({{
 }});
 
 export {{theme}};'''
+
+indexhtml = '''<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link rel="icon" href="./favicon.png" />
+    <title>{}</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
+'''
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def clear_dir(path):
             print('Failed to delete {}. Reason: {}'.format(file_path, e))
 
 class FrontendApp:
+    title = 'App'
     theme = 'light'
     color = '#000'
     contrast_color = '#FFF'
@@ -24,7 +25,8 @@ class FrontendApp:
     models = []
     folder_name = 'frontend' 
 
-    def __init__(self, theme: str, color: str, secondary_color: str, secondary_contrast_color: str, contrast_color: str, components: list, pages: list, models: list):
+    def __init__(self, title: str, theme: str, color: str, secondary_color: str, secondary_contrast_color: str, contrast_color: str, components: list, pages: list, models: list):
+        self.title = title
         self.theme = theme
         self.color = color
         self.contrast_color = contrast_color
@@ -34,23 +36,33 @@ class FrontendApp:
         self.pages = pages
         self.models = models
 
-    #def generate_file(*variables):
-
+    def generate_file(self, path, template, *variables):
+        with open('./result/{}/{}'.format(self.folder_name, path), 'w') as file:
+            text = template.format(*variables)
+            file.write(text)
 
     def init(self):
         clear_dir('./result/{}'.format(self.folder_name))
         copy_tree('./frontend-template', './result/{}'.format(self.folder_name))
 
-    def generate(self):
+    def generate_app(self):
         self.init()
 
-        with open('./result/{}/src/config.js'.format(self.folder_name), 'w') as file:
-            print(self.theme, self.color, self.contrast_color, self.secondary_color, self.secondary_contrast_color)
-            text = formatted.configjs.format(self.theme, self.color, self.contrast_color, self.secondary_color, self.secondary_contrast_color)
-            file.write(text)
+        self.generate_file(
+            'src/config.js', 
+            formatted.configjs,
+            self.theme, self.color, self.contrast_color, self.secondary_color, self.secondary_contrast_color
+        )
         
-frontendApp = FrontendApp('light', '#2c3e50', '#FFF', '#bdc3c7', '#2c3e50', [], [], [])
-frontendApp.generate()
+        self.generate_file(
+            'public/index.html',
+            formatted.indexhtml,
+            self.title
+        )
+
+if __name__ == '__main__':
+    frontendApp = FrontendApp('some App', 'light', '#2c3e50', '#FFF', '#bdc3c7', '#2c3e50', [], [], [])
+    frontendApp.generate_app()
 
 #clear_dir('./result/backend')
 #copy_tree("./backend-template", "./result/backend")
